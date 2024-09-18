54ec4a9e4318f934b54d0c5a4a865b1fe3ae45cf
kelbiyev <kelbiyev11@mail.ru>
2024-09-18 11:12:23 +0400

    Add changes to file example4.cpp

--- example4.cpp
+++ example4.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <utility>
 
+int main() {
+	
+	std::pair<std::string , int> p = {"Hello" , 42};
+	auto[word,freq] = p;
+	std::cout<<word<<" "<<freq<<"\n";
+	return 0;
+	
+}
